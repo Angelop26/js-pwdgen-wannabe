8d595f781bc5b5b910723d996f3f8b483c454706
Angelop26 <angeloprospero26@gmail.com>
2023-03-07 13:55:24 +0100

    project complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-alert('ciao')
+// Chiedere all'utente nome,cognome e colore preferito
+
+const UserName = prompt('Come ti chiami?')
+const UserUsername = prompt('Qual è il tuo cognome?')
+const UserColor = prompt('Qual è il tuo colore preferito?')
+
+console.log(UserName);
+console.log(UserUsername);
+console.log(UserColor);
+
+// Combiniamo le risposte in una password aggiungendo il numero 21 finale
+const UserPassword = UserName + UserUsername + UserColor + '21'
+console.log(UserPassword);
+
+// Stampare il risultato
+document.getElementById('user-password').innerHTML = `La tua password è ${UserPassword}`
